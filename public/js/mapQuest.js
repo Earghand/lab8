c8c31f15f1f56f72390e6480defb532c1abc8d95
Elias <elias@Eliass-MacBook-Pro.local>
2021-02-24 11:37:13 -0800

    Finished Lab8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,10 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'dIfactmSvc7zL6egPfd6wAKYgooL2ici';
+	var map = L.mapquest.map('map', {
+		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	  });
+	  L.marker([32.88, -117.236]).addTo(map);
 }
